5d677522cc79fce831c78f09274ee957609b1d9e
Mingwei Zhang <mingwei@mwzhang.com>
2023-11-26 15:19:08 -0800

    move meta info to dedicated module

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,5 @@
 mod latest_files;
+mod meta;
 mod utils;
 
 use crate::db::utils::infer_url;
@@ -42,17 +43,6 @@ fn get_ts_start_clause(ts: i64) -> String {
 
 fn get_ts_end_clause(ts: i64) -> String {
     format!("timestamp < {}", ts)
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[cfg_attr(feature = "cli", derive(poem_openapi::Object))]
-pub struct UpdatesMeta {
-    /// database update timestamp
-    pub update_ts: i64,
-    /// database update duration in seconds
-    pub update_duration: i32,
-    /// number of items inserted
-    pub insert_count: i32,
 }
 
 impl LocalBrokerDb {
@@ -170,74 +160,6 @@ impl LocalBrokerDb {
             .execute(&self.conn_pool)
             .await
             .unwrap();
-    }
-
-    pub async fn insert_meta(
-        &self,
-        crawl_duration: i32,
-        item_inserted: i32,
-    ) -> Result<Vec<UpdatesMeta>, BrokerError> {
-        debug!("Inserting meta information...");
-        let now_ts = chrono::Utc::now().naive_utc().timestamp();
-        let inserted: Vec<UpdatesMeta> = sqlx::query(&format!(
-            r#"
-            INSERT INTO meta (update_ts, update_duration, insert_count) 
-            VALUES ('{}', {}, {})
-            RETURNING update_ts, update_duration, insert_count
-            "#,
-            now_ts, crawl_duration, item_inserted
-        ))
-        .map(|row: SqliteRow| {
-            let update_ts = row.get::<i64, _>(0);
-            let update_duration = row.get::<i32, _>(1);
-            let insert_count = row.get::<i32, _>(2);
-            UpdatesMeta {
-                update_ts,
-                update_duration,
-                insert_count,
-            }
-        })
-        .fetch_all(&self.conn_pool)
-        .await
-        .unwrap();
-        Ok(inserted)
-    }
-
-    pub async fn get_latest_updates_meta(&self) -> Result<Option<UpdatesMeta>, BrokerError> {
-        let entries = sqlx::query(
-            r#"
-            SELECT update_ts, update_duration, insert_count FROM meta ORDER BY update_ts DESC LIMIT 1;
-            "#,
-        ).map(|row: SqliteRow| {
-            let update_ts = row.get::<i64, _>(0);
-            let update_duration = row.get::<i32, _>(1);
-            let insert_count = row.get::<i32, _>(2);
-            UpdatesMeta {
-                update_ts,
-                update_duration,
-                insert_count,
-            }
-        }).fetch_all(&self.conn_pool).await?;
-        if entries.is_empty() {
-            Ok(None)
-        } else {
-            Ok(Some(entries[0].clone()))
-        }
-    }
-
-    /// Check if data bootstrap is needed
-    #[allow(dead_code)]
-    async fn get_entry_count(&self) -> Result<i64, BrokerError> {
-        let count = sqlx::query(
-            r#"
-            SELECT count(*) FROM files
-            "#,
-        )
-        .map(|row: SqliteRow| row.get::<i64, _>(0))
-        .fetch_one(&self.conn_pool)
-        .await
-        .unwrap();
-        Ok(count)
     }
 
     #[allow(clippy::too_many_arguments)]
